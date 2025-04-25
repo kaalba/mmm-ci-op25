06d8420be90c553a25ec482be73ffc99d2228a1a
kaalba <karen.albarracin96@gmail.com>
2025-04-25 17:09:17 -0500

    Update causal_impact_1.py

--- causal_impact_1.py
+++ causal_impact_1.py
@@ -61,7 +61,7 @@ st.markdown(f"<pre>{impact.summary(output='report')}</pre>", unsafe_allow_html=T
 st.subheader("📈 Impact Plot - All Plots")
 # Plot the results using the CausalImpact plot function
 fig, ax = plt.subplots(figsize=(10, 6))
-impact.plot(ax=ax)
+impact.plot()
 
 # Embed the plot into Streamlit
 st.pyplot(fig)
